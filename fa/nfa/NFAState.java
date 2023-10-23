bab01f4929e131abcbfbb63c474108925ab72235
ChristianGalvan001 <44100003+ChristianGalvan001@users.noreply.github.com>
2023-10-22 20:50:27 -0600

    fixed addTransition and added toStates

--- fa/nfa/NFAState.java
+++ fa/nfa/NFAState.java
@@ -1,11 +1,17 @@
 package fa.nfa;
 
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
 import fa.State;
 
+/**
+ * NFAState object that can specify a set of transitions that a state can take
+ * Date: Sun 22 Oct 2023
+ * @author Christian Galvan, Andrew Bates
+ */
 public class NFAState extends State{
     private Map<Character, Set<NFAState>> transitions;
 
@@ -16,7 +22,11 @@ public class NFAState extends State{
 
     public void addTransition(char onSymb, NFAState toState) {
         Set<NFAState> temp = transitions.get(onSymb);
-        if(temp != null && !temp.contains(toState)) {
+        if (temp == null) {
+            temp = new LinkedHashSet<>();
+            transitions.put(onSymb, temp);
+        }
+        if (!temp.contains(toState)) {
             temp.add(toState);
         }
     }
@@ -24,4 +34,13 @@ public class NFAState extends State{
     public Set<NFAState> getTransition(char onSymb) {
         return transitions.get(onSymb);
     }
+
+    public Set<NFAState> toStates(char onSymb) {
+        Set<NFAState> states = transitions.get(onSymb);
+        if (states == null) {
+            return new LinkedHashSet<>();
+        }
+        return states;
+    }
+
 }
